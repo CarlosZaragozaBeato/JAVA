4c594798f3ab5134eb48e59384650772f9b38332
carlos <carloszaragozabeato15@gmail.com>
2024-12-01 13:38:33 +0100

    Arriva

---
+++ Introduction/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        byte myAge = (byte) (age + 5);
+        long viewsCounts = 142_121_245_515L;
+        double price = 2.99;
+        float price2 = 212.12F;
+
+        System.out.println(myAge);
+        System.out.println(age);
+    }
+}
